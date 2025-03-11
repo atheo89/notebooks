545fb297ef87919299a5c1b1d48271f15965014b
atheo89 <atheodorak@outlook.com>
2025-03-11 18:00:00 +0100

    make one singe job run

--- ci/notebooks-digest-updater.sh
+++ ci/notebooks-digest-updater.sh
@@ -9,6 +9,7 @@ BRANCH=main
 
 # Explicitly check for both cases
 if [[ "$REPO_OWNER" == "opendatahub-io" ]]; then
+
     echo "this is opendatahub-io org"
 
     GITHUB_API_URL="https://api.github.com/repos/$REPO_OWNER/$REPO_NAME"
@@ -28,7 +29,6 @@ if [[ "$REPO_OWNER" == "opendatahub-io" ]]; then
 
     # Get the complete list of images N-version to update
     IMAGES=$(grep "\-n=" "${PARAMS_ENV_PATH}" | cut -d "=" -f 1)
-
     for image in ${IMAGES}; do
 
         echo "CHECKING: '${image}'"
@@ -36,11 +36,14 @@ if [[ "$REPO_OWNER" == "opendatahub-io" ]]; then
         registry=$(echo "${img}" | cut -d '@' -f1)
 
         skopeo_metadata=$(skopeo inspect --retry-times 3 "docker://${img}")
-
         src_tag=$(echo "${skopeo_metadata}" | jq '.Env[] | select(startswith("OPENSHIFT_BUILD_NAME=")) | split("=")[1]' | tr -d '"' | sed 's/-amd64$//')
-        # This should match with the jupyter-minimal-ubi9-python-3.11-20250310-60b6ecc tag name as is on quay.io registry
+        # Handling PyTorch to match theregex pattern
+        if [[ $src_tag == jupyter-pytorch* ]]; then
+           src_tag="cuda-$src_tag"
+        fi
+        echo "src_tag: '${src_tag}'"
+        # This should match ex: jupyter-minimal-ubi9-python-3.11-20250310-60b6ecc tag name as is on quay.io registry
         regex="^$src_tag-\d+-$HASH\$"
-
         latest_tag=$(echo "${skopeo_metadata}" | jq -r --arg regex "$regex" '.RepoTags | map(select(. | test($regex))) | .[0]')
         # use `--no-tags` for skopeo once available in newer version
         digest=$(skopeo inspect --retry-times 3 "docker://${registry}:${latest_tag}" | jq .Digest | tr -d '"')
@@ -61,6 +64,7 @@ if [[ "$REPO_OWNER" == "opendatahub-io" ]]; then
 
 
 elif [[ "$REPO_OWNER" == "red-hat-data-services" ]]; then
+
     echo "this is red-hat-data-services org"
 
     GITHUB_API_URL="https://api.github.com/repos/$REPO_OWNER/$REPO_NAME"
@@ -80,7 +84,6 @@ elif [[ "$REPO_OWNER" == "red-hat-data-services" ]]; then
 
     # Get the complete list of images N-version to update
     IMAGES=$(grep "\-n=" "${PARAMS_ENV_PATH}" | cut -d "=" -f 1)
-
     # The order of the regexes array should match with the params.env file
     REGEXES=("v3-\d{8}-+$HASH" \
               "cuda-[a-z]+-minimal-[a-z0-9]+-[a-z]+-3.11-\d{8}-$HASH" \
@@ -92,7 +95,7 @@ elif [[ "$REPO_OWNER" == "red-hat-data-services" ]]; then
               "rocm-[a-z]+-minimal-[a-z0-9]+-[a-z]+-3.11-\d{8}-$HASH" \
               "rocm-[a-z]+-pytorch-[a-z0-9]+-[a-z]+-3.11-\d{8}-$HASH" \
               "rocm-[a-z]+-tensorflow-[a-z0-9]+-[a-z]+-3.11-\d{8}-$HASH")
-          i=0
+    i=0
     for image in ${IMAGES}; do
         echo "CHECKING: '${image}'"
         img=$(grep -E "${image}=" "${PARAMS_ENV_PATH}" | cut -d '=' -f2)
@@ -108,18 +111,17 @@ elif [[ "$REPO_OWNER" == "red-hat-data-services" ]]; then
         i=$((i+1))
     done
 
-  COMMIT_ENV_PATH="$REPO_ROOT/manifests/base/commit.env"
+    COMMIT_ENV_PATH="$REPO_ROOT/manifests/base/commit.env"
 
-  # Get the complete list of commits N-version to update
-  COMMIT=$(grep "\-n=" "${COMMIT_ENV_PATH}" | cut -d "=" -f 1)
+    # Get the complete list of commits N-version to update
+    COMMIT=$(grep "\-n=" "${COMMIT_ENV_PATH}" | cut -d "=" -f 1)
 
-  for val in ${COMMIT}; do
-    echo "${val}"
-    sed -i "s|${val}=.*|${val}=$HASH|" "${COMMIT_ENV_PATH}"
-  done
-
+    for val in ${COMMIT}; do
+      echo "${val}"
+      sed -i "s|${val}=.*|${val}=$HASH|" "${COMMIT_ENV_PATH}"
+    done
 
 else
-    echo "This script runs exclusively for the 'opendatahub-io' and 'red-hat-datascience' organizations, as it verifies their corresponding quay.io registries."
+    echo "This script runs exclusively for the 'opendatahub-io' and 'red-hat-datascience' organizations, as it verifies/updates their corresponding quay.io registries."
 fi
 
