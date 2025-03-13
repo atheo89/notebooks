28804043620a7cb2bb90fce788a610c30f50c366
atheo89 <atheodorak@outlook.com>
2025-03-13 16:31:43 +0100

    Update runtimes old stracture ci

---
+++ ci/update-runtimes-old-structure.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+USER_HASH=$1
+REPO_OWNER=$2
+REPO_NAME="notebooks"
+BRANCH=main
+
+# Function to fetch the latest commit hash
+fetch_latest_hash() {
+    local org="$1"
+    local api_url="https://api.github.com/repos/$org/$REPO_NAME"
+
+    if [[ -n "$USER_HASH" ]]; then
+        HASH=$USER_HASH
+        echo "Using user-provided HASH: $HASH"
+    else
+        PAYLOAD=$(curl --silent -H 'Accept: application/vnd.github.v4.raw' "$api_url/commits?sha=$BRANCH&per_page=1")
+        HASH=$(echo "$PAYLOAD" | jq -r '.[0].sha' | cut -c1-7)
+        echo "Extracted HASH: $HASH"
+    fi
+}
+
+# Function to process runtime images
+process_runtime_images() {
+    find . -name runtime-images -type d -exec find {} -type f -print \; | grep python-3.11 | while read -r path; do
+        echo "##############################"
+        echo "Processing the '${path}' file."
+
+        img=$(jq -r '.metadata.image_name' "${path}")
+        echo "DEBUG the img '${img}'."
+
+        name=$(echo "$path" | sed 's#.*runtime-images/\(.*\)-py.*#\1#')
+        echo "DEBUG the name '${name}' "
+
+        py_version=$(echo "$path" | grep -o 'python-[0-9]\.[0-9]*')
+        echo "DEBUG the py_version '${py_version}'."
+
+        registry=$(echo "$img" | cut -d '@' -f1)
+        echo "DEBUG the registry '${registry}' ."
+
+        # Handling specific name cases
+        if [[ $name == tensorflow* || $name == pytorch* ]]; then
+            name="cuda-$name"
+        elif [[ $name == ubi* ]]; then
+            name="minimal-$name"
+        elif [[ $name == rocm-pytorch* ]]; then #rocm-pytorch-ubi9 #rocm-runtime-tensorflow-ubi9-python-3.11
+            name="${name/rocm-pytorch/rocm-runtime-pytorch}"
+        elif [[ $name == rocm-tensorflow* ]]; then #rocm-pytorch-ubi9 #rocm-runtime-tensorflow-ubi9-python-3.11
+            name="${name/rocm-tensorflow/rocm-runtime-tensorflow}"
+        fi
+        if [[ $name == rocm-* ]]; then
+            regex="^$name-$py_version-\d+-$HASH\$"
+            echo "DEBUG the regex '${regex}' ."
+        else
+            regex="^runtime-$name-$py_version-\d+-$HASH\$"
+            echo "DEBUG the regex '${regex}' ."
+        fi
+
+        latest_tag=$(skopeo inspect --retry-times 3 "docker://$img" | jq -r --arg regex "$regex" '.RepoTags | map(select(. | test($regex))) | .[0]')
+        echo "DEBUG the latest_tag '${latest_tag}' "
+
+        echo "CHECKING: ${latest_tag}"
+        if [[ -z "${latest_tag}" ]]; then
+            echo "No matching tag found"
+            exit 1
+        fi
+
+        digest=$(skopeo inspect --retry-times 3 "docker://$registry:$latest_tag" | jq .Digest | tr -d '"')
+        echo "DEBUG the '${path}'."
+
+        output="${registry}@${digest}"
+        echo "DEBUG the output'${output}' ."
+
+
+        echo "NEW: ${output}"
+        jq --arg output "$output" '.metadata.image_name = $output' "$path" > "$path.tmp" && mv "$path.tmp" "$path"
+    done
+}
+
+if [[ "$REPO_OWNER" == "opendatahub-io" ]]; then
+    echo "This is opendatahub-io org"
+    fetch_latest_hash "$REPO_OWNER"
+    process_runtime_images
+
+elif [[ "$REPO_OWNER" == "red-hat-data-services" ]]; then
+    echo "This is red-hat-data-services org"
+    fetch_latest_hash "$REPO_OWNER"
+    process_runtime_images
+
+else
+    echo "This script runs exclusively for the 'opendatahub-io' and 'red-hat-datascience' organizations, as it verifies/updates their corresponding quay.io registries."
+fi
+
