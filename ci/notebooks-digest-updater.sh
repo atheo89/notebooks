a4b2606ca3016c5f96f39498aa9c3d871d07c2d3
Adriana Theodorakopoulou <atheodorak@outlook.com>
2025-04-15 16:14:19 +0200

    merge also the updater scripts to one

--- ci/notebooks-digest-updater.sh
+++ ci/notebooks-digest-updater.sh
@@ -5,6 +5,12 @@ REPO_OWNER=$2
 BRANCH=$3
 REPO_NAME=$4
 
+REPO_ROOT=$(git rev-parse --show-toplevel)
+# Declare and initialize(as the notebooks-digets-updater.sh gets in action first on the gha) the image-skipping-logger.sh 
+export SKIPPED_LOG_PATH="$REPO_ROOT/skipped-images.txt"
+source "$REPO_ROOT/ci/image-skipping-logger.sh"
+init_skipped_log
+
 # Fetch the latest commit hash (or use the user-provided one)
 fetch_latest_hash() {
     local org="$1"
@@ -36,14 +42,74 @@ update_commits() {
     done
 }
 
-REPO_ROOT=$(git rev-parse --show-toplevel)
+# Function to process runtime images
+update_runtime_images() {
+    MANIFEST_DIR="$REPO_ROOT/manifests/base"
+    # Find matching files
+    files=$(find "$MANIFEST_DIR" -type f -name "runtime-*.yaml")
+    for file in $files; do
+        echo "PROCESSING: $file"
+
+        # Extract values
+        img=$(yq e '.spec.tags[].annotations."opendatahub.io/runtime-image-metadata" | fromjson | .[].metadata.image_name' "$file" 2>/dev/null)
+        name=$(yq e '.spec.tags[].name' "$file" 2>/dev/null)
+        ubi=$(yq e '.metadata.annotations."opendatahub.io/runtime-image-name"' "$file" 2>/dev/null | grep -oE 'UBI[0-9]+' | tr '[:upper:]' '[:lower:]')
+        py_version=$(yq e '.metadata.annotations."opendatahub.io/runtime-image-name"' "$file" 2>/dev/null | grep -oE 'Python [0-9]+\.[0-9]+' | sed 's/ /-/g' | tr '[:upper:]' '[:lower:]')
+        registry=$(echo "$img" | cut -d '@' -f1)
+
+        # Handling specific cases
+        if [[ $name == tensorflow || $name == pytorch ]]; then
+            name="cuda-$name"
+        elif [[ $name == ubi ]]; then
+            name="minimal-$name"
+        elif [[ $name == rocm-pytorch ]]; then
+            name="${name/rocm-pytorch/rocm-runtime-pytorch}"
+        elif [[ $name == rocm-tensorflow ]]; then
+            name="${name/rocm-tensorflow/rocm-runtime-tensorflow}"
+        fi
+
+        # Construct regex pattern
+        prefix="runtime-"
+        [[ $name == rocm-* ]] && prefix=""
+        regex="^${prefix}$name-$ubi-$py_version-[0-9]{8}-$HASH$"
+
+        latest_tag=$(skopeo inspect --retry-times 3 "docker://$img" | jq -r --arg regex "$regex" '.RepoTags | map(select(. | test($regex))) | .[0]')
+        echo "CHECKING: ${latest_tag}"
+
+        # Check for latest_tag validity (maybe the new image is not yet built)
+        if [[ -z "$latest_tag" || "$latest_tag" == "null" ]]; then
+            echo "No matching tag found on registry for $file. Skipping."
+            # Get relative path from REPO_ROOT/ci to the file
+            relative_path=$(echo "$file" | sed "s|$REPO_ROOT/||")
+            # calls log_skipped_image funtion from image-skipping-logger.sh script
+            log_skipped_image "../$relative_path"
+            continue
+        fi
+
+        # Extract the digest sha from the latest tag
+        digest=$(skopeo inspect --retry-times 3 "docker://$registry:$latest_tag" | jq .Digest | tr -d '"')
+
+        # Check for digest validity
+        if [[ -z "$digest" || "$digest" == "null" ]]; then
+            echo "Failed to get digest for $latest_tag. Skipping."
+            # Get relative path from REPO_ROOT/ci to the file
+            relative_path=$(echo "$file" | sed "s|$REPO_ROOT/||")
+            # calls log_skipped_image funtion from image-skipping-logger.sh script
+            log_skipped_image "../$relative_path"
+            continue
+        fi
+
+        output="${registry}@${digest}"
+        echo "NEW: ${output}"
+
+        # Updates the ImageStream with the new SHAs
+        yq e -i '(.spec.tags[] | .from.name) = "'"$output"'"' "$file"
+        sed -i "s|\(\"image_name\": \"\)[^\"]*|\1${output}|" "$file"
+
+    done
+}
+
 PARAMS_ENV_PATH="$REPO_ROOT/manifests/base/params.env"
-
-# Declare and initialize(as the notebooks-digets-updater.sh gets in action first on the gha) the image-skipping-logger.sh 
-export SKIPPED_LOG_PATH="$REPO_ROOT/skipped-images.txt"
-source "$REPO_ROOT/ci/image-skipping-logger.sh"
-init_skipped_log
-
 # In case the digest updater function is triggered upstream.
 if [[ "$REPO_OWNER" == "opendatahub-io" ]]; then
 
@@ -91,6 +157,7 @@ if [[ "$REPO_OWNER" == "opendatahub-io" ]]; then
     done
 
     update_commits "$REPO_ROOT" "$HASH"
+    update_runtime_images
 
 # In case the digest updater function is triggered downstream.
 elif [[ "$REPO_OWNER" == "red-hat-data-services" ]]; then
@@ -145,6 +212,7 @@ elif [[ "$REPO_OWNER" == "red-hat-data-services" ]]; then
     done
 
     update_commits "$REPO_ROOT" "$HASH"
+    update_runtime_images
 
 else
     echo "This script runs exclusively for the 'opendatahub-io' and 'red-hat-datascience' organizations, as it verifies/updates their corresponding quay.io registries."
