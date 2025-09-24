39dbb2bac0201d66678a7369b7512bf2fabd779e
atheo89 <atheodorak@outlook.com>
2025-09-24 10:57:56 +0200

    Update python refs and rstudio rpm version

--- rstudio/c9s-python-3.12/install_packages.R
+++ rstudio/c9s-python-3.12/install_packages.R
@@ -9,6 +9,5 @@ if (!requireNamespace("remotes", quietly = TRUE)) {
 remotes::install_version('Rcpp', '1.0.14')
 remotes::install_version('tidyverse', '2.0.0')
 remotes::install_version('tidymodels', '1.3.0')
-remotes::install_version('plumber', '1.3.0')
 remotes::install_version('vetiver', '0.2.5')
 remotes::install_version('devtools', '2.4.5')
